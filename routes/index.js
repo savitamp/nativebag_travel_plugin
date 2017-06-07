cf7e05c3b435cacdefeea2a52678ea6f3ab2298c
krishna kanth mundada <es14btech11013@iith.ac.in>
2017-06-07 18:09:41 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,8 @@ const session = require('express-session');
 const app = express({mergeParams: true});
 
 
-
-// app.get('/:place/:id([0-9]{1,})', function(req, res){
-//     if (products[0][req.params.place] === undefined)
-//         res.send("place doesnt exist");
-//     else {
-//         if (products[0][req.params.place][req.params.id - 1] === undefined)
-//             res.send ("id doesnt exist");
-//         else {
-//             console.log(products[0][req.params.place][req.params.id-1]);
-//             res.render('small_div.pug', {product_name:products[0][req.params.place][req.params.id-1]});
-//         }
-//     }
-// });
+var cart;
+var sid = null;
 
 app.use(function (req, res, next) {
 
@@ -26,7 +15,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 
     // Request headers you wish to allow
     // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -42,41 +31,90 @@ app.use(function (req, res, next) {
 
 app.use(session({secret: 'ssshhhhh'}));
 
-app.get('/:place', function (req, res) {
+app.get('/summary', function (req, res) {
+    res.render('summary.pug')
+});
 
-    const sid = req.query.sid;
-    request('http://api.test.nativebag.in/v1/product/exists?locationA=Tuticorin&locationB='+req.params.place+'&sid='+sid,
-        function (error, response, body) {
+app.get('/product_list/:place', function (req, res) {
+    sid = req.query.sid;
+    var location =  req.params.place;
+    var products;
+    var cart_items;
+    var cart_ids = [];
+    request.get('http://api.test.nativebag.in/v1/product/exists?locationA=Tuticorin&locationB='+req.params.place+'&sid='+sid,
+        function (error, response, product_list) {
         if (!error && response.statusCode == 200) {
-            body = JSON.parse(body);
+            product_list = JSON.parse(product_list);
+            products = product_list.products;
+            request.post({
+                url: 'http://api.test.nativebag.in/v1/cart/cart-items',
+                form: {sid:sid}
+                }, function (error, response, body) {
+                    body = JSON.parse(body);
+                    console.log(body);
+                    if(body.CartItems !== null) {
+                        const totalMessages = Object.keys(body.CartItems).length;
+                        for (var i = 0; i < totalMessages; i++) {
+                            cart_ids.push(body.CartItems[i].ProductId);
+                        }
+                        console.log(cart_ids);
+                    }
+                    cart_items = body.CartItems;
+                    cart_ids = JSON.stringify(cart_ids);
+                    res.render('native.pug', {location:location, product_name:products,
+                        cart_items:cart_ids, cart_products:cart_items, item_count:body.count, amount: body.totalAmount, sid: sid});
 
-            console.log(body.cart);
-            var cart_ids = [];
-            if(body.cart != null) {
-                const totalMessages = Object.keys(body.cart).length;
-                for (var i = 0; i < totalMessages; i++) {
-                    // console.log("ID: " + body.cart[i].product_id);
-                    cart_ids.push(body.cart[i].product_id);
-                    // console.log(cart_ids);
-                    console.log(JSON.stringify(cart_ids));
-                }
-            }
-            console.log(body.count);
-            cart_ids = JSON.stringify(cart_ids);
-            res.render('native.pug', {location: req.params.place, product_name: body.products,
-                cart_items:cart_ids, cart_products:body.cart, item_count:body.count, amount: body.totalAmount, sid: sid});
+            });
+
+            // cart = body.cart;
         }
     });
 });
+
+app.get('/cart',function(req, res) {
+    request.post({
+        url: 'http://api.test.nativebag.in/v1/cart/cart-items',
+        form: {sid:sid}
+    }, function (error, response, body) {
+        if(!error && response.statusCode === 200) {
+            var cart_ids = [];
+            if (body.CartItems !== null) {
+                console.log(body.CartItems);
+                // const totalMessages = Object.keys(body.CartItems).length;
+                // for (var i = 0; i < totalMessages; i++) {
+                //     cart_ids.push(body.CartItems[i].ProductId);
+                // }
+            }
+            body = JSON.parse(body);
+            console.log(body.CartItems);
+            res.send(body.CartItems);
+        }
+    });
+});
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-app.post('/id', function (req, res) {
 
-    console.log(req.body.someInput);
-
-    return res.json({data:"success111"});
-    // console.log(req.data());
+app.get('/sid', function (req, res) {
+    if(sid !== null)
+        return res.json({sid:sid});
+    else return res.send('error');
 });
+
+
+// app.get('/:place/:id([0-9]{1,})', function(req, res){
+//     if (products[0][req.params.place] === undefined)
+//         res.send("place doesnt exist");
+//     else {
+//         if (products[0][req.params.place][req.params.id - 1] === undefined)
+//             res.send ("id doesnt exist");
+//         else {
+//             console.log(products[0][req.params.place][req.params.id-1]);
+//             res.render('small_div.pug', {product_name:products[0][req.params.place][req.params.id-1]});
+//         }
+//     }
+// });
+
 
 module.exports = app;
